6c334f4264806cbc7408906eaac73f624d17749e
Ivan Herhi <ionherhi@gmail.com>
2025-05-12 19:15:28 +0200

    version-01

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,153 @@
-/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
+/* eslint-disable max-len */
 
-const USER_ID = 0;
+// #region Components
+
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { TodoList } from './components/TodoList';
+import { MessageError } from './components/MessageError';
+
+// #endregion
+
+// #region allImport
+
+import { Todo } from './types/Todo';
+import { getTodos, postTodos, deleteTodos } from './api/todos';
+import React, { useEffect, useRef, useState } from 'react';
+
+// #endregion
+
+export type TypeNewTodo = {
+  id: null;
+  userId: number;
+  title: string;
+  completed: boolean;
+};
+
+const USER_ID = 2881;
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  // #region State
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [hasError, setHasError] = useState(false);
+  const [theError, setTheError] = useState<string>('');
+  const [inputValue, setInputValue] = useState<string>('');
+  const [NewTodo, setCreateNewTodo] = useState<TypeNewTodo | null>(null);
+  const [filter, setFilter] = useState<'all' | 'active' | 'completed'>('all');
+
+  // #endregion
+
+  // #region showErro
+
+  const errorTimerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const showError = (message: string) => {
+    setHasError(true);
+    setTheError(message);
+
+    if (errorTimerRef.current) {
+      clearTimeout(errorTimerRef.current);
+    }
+
+    errorTimerRef.current = setTimeout(() => {
+      setHasError(false);
+    }, 3000);
+  };
+
+  // #endregion
+
+  // #region DoFetch
+
+  const doPost = (titleValue: string) => {
+    if (titleValue.length === 0) {
+      showError('Title should not be empty');
+
+      return;
+    }
+
+    const createNewTodo: TypeNewTodo = {
+      id: null,
+      userId: USER_ID,
+      title: titleValue,
+      completed: false,
+    };
+
+    setCreateNewTodo(createNewTodo);
+
+    postTodos(createNewTodo.userId, createNewTodo)
+      .then(response => {
+        setTodos(prev => [response, ...prev]);
+        setInputValue('');
+        setCreateNewTodo(null);
+      })
+      .catch(() => {
+        showError('Unable to add a todo');
+      });
+  };
+
+  const deletePost = (id: number) => {
+    deleteTodos(id)
+      .then(() => {
+        setTodos(todos.filter(todo => todo.id !== id));
+      })
+      .catch(() => {
+        showError('Unable to delete a todo');
+      });
+  };
+
+  // #endregion
+
+  const visibleTodos = todos.filter(todo => {
+    if (filter === 'active') {
+      return !todo.completed;
+    }
+
+    if (filter === 'completed') {
+      return todo.completed;
+    }
+
+    return true;
+  });
+
+  useEffect(() => {
+    getTodos(USER_ID)
+      .then(response => {
+        setTodos(response);
+      })
+      .catch(() => {
+        showError('Unable to load todos');
+      });
+  }, []);
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header
+          doPost={doPost}
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+        />
+
+        <TodoList
+          todos={todos}
+          NewTodo={NewTodo}
+          deletePost={deletePost}
+          visibleTodos={visibleTodos}
+        />
+
+        <Footer setFilter={setFilter} filter={filter} todos={todos} />
+      </div>
+
+      <MessageError
+        setHasError={setHasError}
+        hasError={hasError}
+        theError={theError}
+      />
+    </div>
   );
 };
